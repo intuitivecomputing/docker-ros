b89ea3b27a6df83599c2b9bf02369803065f4ffd
Yuxiang Gao <davidgogh96@gmail.com>
2019-03-30 18:36:33 -0400

    changes

---
+++ docker/docker-ros-realsense/ros_entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+machine_ip=(`hostname -I`) 
+echo "export ROS_IP=${machine_ip[0]}" >> /root/.bashrc
+echo "export ROS_MASTER_URI=http://localhost:11311" >> /root/.bashrc
+
+# setup ros environment
+if [ -z "${SETUP}" ]; then
+    source "/catkin_ws/devel/setup.bash"
+else
+    source $SETUP
+fi
+
+exec "$@"

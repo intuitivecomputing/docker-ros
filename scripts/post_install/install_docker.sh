4b4a8859f0beb7625658af1c455739b5cc66ff71
Yuxiang Gao <yuxianggao@Yuxiangs-MacBook-Pro.local>
2019-04-23 15:33:50 -0400

    docker setup

---
+++ scripts/post_install/install_docker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -ax
+
+function install_docker {
+    sudo apt-get remove docker docker-engine docker.io containerd runc
+    sudo apt-get update
+    sudo apt-get install \
+                apt-transport-https \
+                ca-certificates \
+                curl \
+                gnupg-agent \
+                software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo apt-key fingerprint 0EBFCD88
+    sudo add-apt-repository \
+        "deb [arch=arm64] https://download.docker.com/linux/ubuntu \
+        $(lsb_release -cs) \
+        stable"
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io
+    sudo usermod -aG docker ${USER}
+}
+
+function install_docker_compose {
+    sudo apt-get install -y libffi-dev python-openssl
+    curl -sSL https://bootstrap.pypa.io/get-pip.py | sudo python
+    sudo pip install docker-compose
+}
+
+function main {
+    install_docker
+    install_docker_compose
+    sudo docker run --rm -it arm64v8/ubuntu:16.04 cat /etc/issue
+    sudo shutdown -r now
+}

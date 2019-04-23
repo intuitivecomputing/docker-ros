e66d44a79418eb53a6fa50dbbcc5cfdde1a21711
Yuxiang Gao <yuxianggao@jhu.edu>
2019-04-23 16:12:25 -0400

    set ROS env vars

--- scripts/post_install/setup.sh
+++ scripts/post_install/setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ax
 
+echo "export ROS_MASTER_URI=http://192.168.1.116:11311" > ${HOME}/.bashrc
+echo "export ROS_IP=$(hostname -I | cut -f1 -d' ')" > ${HOME}/.bashrc
+
 sudo apt update
 sudo apt install -qqy fabric
 cp txdocker /usr/local/bin/

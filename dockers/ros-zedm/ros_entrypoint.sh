847eb648d252ddbde79c01e04a95888dce8fcd90
Annie Mao <annie.m.mao@gmail.com>
2019-10-27 23:35:53 -0400

    preliminary Dockerfile for ros-zedm image on TX2 (arm64 arch) + docker-compose file for zedm and realsense

---
+++ dockers/ros-zedm/ros_entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+# machine_ip=(`hostname -I`)
+# echo "export ROS_IP=${machine_ip[0]}" >> /root/.bashrc
+# echo "export ROS_MASTER_URI=http://localhost:11311" >> /root/.bashrc
+
+# setup ros environment
+# SETUP="/opt/ros/$ROS_DISTRO/setup.bash"
+
+if [ -z "${SETUP}" ]; then
+    source "/catkin_ws/devel/setup.bash"
+    echo "source /catkin_ws/devel/setup.bash" >> $HOME/.bashrc
+else
+    source $SETUP
+fi
+
+exec "$@"
